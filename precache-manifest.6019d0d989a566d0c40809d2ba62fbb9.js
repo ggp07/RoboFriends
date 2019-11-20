a4a4df505a4cad7df432db60a2bd6767bb0f1e67
unknown <bab.markos@gmail.com>
2019-11-20 19:24:23 +0200

    Updates

---
+++ precache-manifest.6019d0d989a566d0c40809d2ba62fbb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049e7833d61808eb73ef29a89973012b",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5db033fe626a0c7e7acf",
+    "url": "/RoboFriends/static/css/main.a6dac6f4.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/RoboFriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "5db033fe626a0c7e7acf",
+    "url": "/RoboFriends/static/js/main.87493c9d.chunk.js"
+  },
+  {
+    "revision": "6239f40d0ef81a2fd1e0",
+    "url": "/RoboFriends/static/js/runtime-main.6d5bc549.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
